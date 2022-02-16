3463cbae459c272f45efb1e2de1e33d1445f0c27
all-an <all-an@hotmail.com>
2022-02-15 21:39:17 -0300

    iniciando

---
+++ src/main/java/com/listaempresas/BackendListaEmpresasSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.listaempresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendListaEmpresasSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendListaEmpresasSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listaempresas/BackendListaEmpresasSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listaempresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendListaEmpresasSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
